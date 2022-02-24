8beb66fc060e06e0cbbc3621fd29834f24e5c37f
Yasiru Dahanayaka <yasirudahanayaka19@gmail.com>
2022-02-24 20:38:08 +0530

    Item Text Validated

--- assests/js/javaScript.js
+++ assests/js/javaScript.js
@@ -256,22 +256,22 @@ function formValid() {
                     return true;
                 } else {
                     $("#txtCusContact").css('border', '2px solid red');
-                    $("#lblCusContact").text("Cus Contact is a required field : Pattern 076-8383493");
+                    $("#lblCusContact").text("Customer Contact is a required field : Pattern 076-8383493");
                     return false;
                 }
             } else {
                 $("#txtCusAddress").css('border', '2px solid red');
-                $("#lblCusAddress").text("Cus Name is a required field : Minimum 7");
+                $("#lblCusAddress").text("Customer Name is a required field : Minimum 7");
                 return false;
             }
         } else {
             $("#txtCusName").css('border', '2px solid red');
-            $("#lblCusName").text("Cus Name is a required field : Minimum 5, Max 20, Spaces Allowed");
+            $("#lblCusName").text("Customer Name is a required field : Minimum 5, Max 20, Spaces Allowed");
             return false;
         }
     } else {
         $("#txtCusId").css('border', '2px solid red');
-        $("#lblCusId").text("Cus ID is a required field : Pattern C00-000");
+        $("#lblCusId").text("Customer ID is a required field : Pattern C00-000");
         return false;
     }
 }
@@ -395,6 +395,141 @@ $('#txtItemCode,#txtItemName,#txtItemQuantity,#txtItemUnitPrice').on('keydown', 
 $('#txtItemCode,#txtItemName,#txtItemQuantity,#txtItemUnitPrice').on('blur', function (){
     formItemValid();
 });
+
+$("#txtItemCode").on('keyup', function (eventOb){
+    setItemButton();
+    if (eventOb.key == "Enter"){
+        checkIfItemValid();
+    }
+    if (eventOb.key == "Control"){
+        var typedItemCode = $("#txtItemCode").val();
+        var srcItem = searchItemFromCode(typedItemCode);
+        $("#txtItemCode").val(srcItem.getItemCode());
+        $("#txtItemName").val(srcItem.getItemName());
+        $("#txtItemQuantity").val(srcItem.getItemQty());
+        $("#txtItemUnitPrice").val(srcItem.getItemUnitPrice());
+
+    }
+});
+
+$("#txtItemName").on('keyup', function (eventOb){
+    setItemButton();
+    if (eventOb.key == "Enter"){
+        checkIfItemValid();
+    }
+});
+
+$("#txtItemQuantity").on('keyup', function (eventOb){
+    setItemButton();
+    if (eventOb.key == "Enter"){
+        checkIfItemValid();
+    }
+});
+
+$("#txtItemUnitPrice").on('keyup', function (eventOb){
+    setItemButton();
+    if (eventOb.key == "Enter"){
+        checkIfItemValid();
+    }
+});
+
+$("#btnItemSave").attr('disabled', true);
+
+function clearItemAll() {
+    $('#txtItemCode,#txtItemName,#txtItemQuantity,#txtItemUnitPrice').val("");
+    $('#txtItemCode,#txtItemName,#txtItemQuantity,#txtItemUnitPrice').css('border', '2px solid #ced4da');
+    $('#txtItemCode').focus();
+    $("#btnItemSave").attr('disabled', true);
+    loadAllItem();
+    $("#lblItemId,#lblItemName,#lblItemQty,#lblItemUnitPrice").text("");
+}
+
+function formItemValid(){
+    var itemCode = $("#txtItemCode").val();
+    $("#txtItemCode").css('border', '2px solid green');
+    $("#lblItemId").text("");
+    if (itemIdRegEx.test(itemCode)){
+        var itemName = $("#txtItemName").val();
+        if (itemNameRegEx.test(itemName)) {
+            $("#txtItemName").css('border', '2px solid green');
+            $("#lblItemName").text("");
+            var itemQty = $("#txtItemQuantity").val();
+            if (itemQtyRegEx.test(itemQty)) {
+                var itemPrice = $("#txtItemUnitPrice").val();
+                var resp = itemPriceRegEx.test(itemPrice);
+                $("#txtItemQuantity").css('border', '2px solid green');
+                $("#lblItemQty").text("");
+                if (resp) {
+                    $("#txtItemUnitPrice").css('border', '2px solid green');
+                    $("#lblItemUnitPrice").text("");
+                    return true;
+                } else {
+                    $("#txtItemUnitPrice").css('border', '2px solid red');
+                    $("#lblItemUnitPrice").text("Item Unit Price is a required field : Pattern 100.0");
+                    return false;
+                }
+            } else {
+                $("#txtItemQuantity").css('border', '2px solid red');
+                $("#lblItemQty").text("Item Quantity is a required field : Minimum 2");
+                return false;
+            }
+        } else {
+            $("#txtItemName").css('border', '2px solid red');
+            $("#lblItemName").text("Item Name is a required field : Minimum 5, Max 20, Spaces Allowed");
+            return false;
+        }
+    } else {
+        $("#txtItemCode").css('border', '2px solid red');
+        $("#lblItemId").text("Item Code is a required field : Pattern I00-000");
+        return false;
+    }
+}
+
+function checkIfItemValid() {
+    var itemCode = $("#txtItemCode").val();
+    if (itemIdRegEx.test(itemCode)) {
+        $("#txtItemName").focus();
+        var itemName = $("#txtItemName").val();
+        if (itemNameRegEx.test(itemName)) {
+            $("#txtItemQuantity").focus();
+            var itemQty = $("#txtItemQuantity").val();
+            if (itemQtyRegEx.test(itemQty)) {
+                $("#txtItemUnitPrice").focus();
+                var itemPrice = $("#txtItemUnitPrice").val();
+                var resp = itemPriceRegEx.test(itemPrice);
+                if (resp) {
+                    let res = confirm("Do you really need to add this Customer..?");
+                    if (res) {
+                        saveItem();
+                        clearItemAll();
+                    }
+                } else {
+                    $("#txtItemUnitPrice").focus();
+                }
+            } else {
+                $("#txtItemQuantity").focus();
+            }
+        } else {
+            $("#txtItemName").focus();
+        }
+    } else {
+        $("#txtItemCode").focus();
+    }
+}
+
+function setItemButton(){
+    let i = formItemValid();
+    if (i) {
+        $("#btnItemSave").attr('disabled', false);
+    }else{
+        $("#btnItemSave").attr('disabled', true);
+    }
+}
+
+$("#btnItemSave").click(function (){
+    checkIfItemValid();
+})
+
 
 
 
